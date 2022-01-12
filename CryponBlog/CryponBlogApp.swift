188e15e3d8578518328152342a2e0cffa3ada1b1
Furkan Başoğlu <furkan.basoglu@std.medipol.edu.tr>
2022-01-12 11:58:55 +0300

    Initial Commit

---
+++ CryponBlog/CryponBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryponBlogApp.swift
+//  CryponBlog
+//
+//  Created by Furkan Başoğlu on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryponBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
