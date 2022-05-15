bc8476799bed10681cdee8330af447bdaf5893d5
MING-TA YANG <startover205@gmail.com>
2022-05-15 21:51:11 +0800

    Update `unlockAllPosts` to a constant since it's not changed in `AllPosts`

--- CryponBlog/Core/Blog/ViewModels/AllPosts.swift
+++ CryponBlog/Core/Blog/ViewModels/AllPosts.swift
@@ -11,7 +11,7 @@ struct AllPosts: View {
     @EnvironmentObject var store: BlogPostsStore
     @Binding var premiumBS: BottomSheetPremium
     @StateObject var storeManager: StoreManager
-    var unlockAllPosts: Bool
+    let unlockAllPosts: Bool
     
     var body: some View {
         NavigationView {
